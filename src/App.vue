6bf73fdcf64c3412d125082e85ab19e3e2358557
Michael Huang <z996345@gmail.com>
2024-08-25 21:09:19 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import VueComponent from './components/VueComponent.vue'
+</script>
+
+<template>
+  <h1>目前是App.vue</h1>
+  <VueComponent></VueComponent>
+</template>
+
+<style></style>
---
+++ src/components/VueComponent.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>這是第一個元件</div>
+</template>
